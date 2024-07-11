0b51468968d2b576b3be350440b222ae8ac2c08f
tyrant0407 <amankushwaha1331@gmail.com>
2024-07-11 16:41:13 +0530

    ScrollAnimationDone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePage from './components/homepage/HomePage'
+import Page2 from './components/page2/Page2'
 
 const App = () => {
   return (
     <div>
       <HomePage/>
+      <Page2/>
     </div>
   )
 }
--- src/components/homepage/heroComponents/HeroText.jsx
+++ src/components/homepage/heroComponents/HeroText.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import { motion, useScroll } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
 
 const HeroText = () => {
+  const {scrollY} = useScroll();
+  const [direction, setdirection] = useState(1)
+
+  const handleSrolling = ()=>{
+     const current = scrollY.get()-scrollY.getPrevious();
+     if(current>0){
+      setdirection(1)
+     }
+     else{
+      setdirection(-1)
+     }
+  }
+    useEffect(() => {
+      scrollY.on("change",handleSrolling)
+    }, [scrollY])
+    
   return (
-    <div className='relative flex mt-8'>
-        <h1 className='text-[14.5vw] font-light whitespace-nowrap text-white'>-Aman Kushwaha</h1>
-        <h1 className='text-[14.5vw] font-light whitespace-nowrap text-white'>-Aman Kushwaha</h1>
-        <h1 className='text-[14.5vw] font-light whitespace-nowrap text-white'>-Aman Kushwaha</h1>
-        <h1 className='text-[14.5vw] font-light whitespace-nowrap text-white'>-Aman Kushwaha</h1>
-    </div>
+    <motion.div 
+    className='relative flex mt-0'
+    initial={{
+        x:-2075,
+    }}
+    animate={{
+    x:direction>0?-4150:0,
+        
+    }}
+    transition={{
+        duration:6,
+        ease:'linear',
+        repeat:Infinity,
+    }}
+    
+    >
+        <h1 className='text-[14.5vw] font-light tracking-tight whitespace-nowrap text-white'>-AmanKushwaha</h1>
+        <h1 className='text-[14.5vw] font-light tracking-tight whitespace-nowrap text-white'>-AmanKushwaha</h1>
+        <h1 className='text-[14.5vw] font-light tracking-tight whitespace-nowrap text-white'>-AmanKushwaha</h1>
+        
+    </motion.div>
   )
 }
 
